b1738d984b32baf98f46bde66c61368468850ff0
declanosullivan <87386022+declanosullivan@users.noreply.github.com>
2024-05-06 08:57:41 +0100

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Domain Central';
+export const SITE_DESCRIPTION = 'Welcome to Domain Central - where every domain leads to new possibilities.';
